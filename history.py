ceae6e0a46195905bda5db911339670914028aa6
Rutuja Pimpalgaonkar <89966183+rutujapimpalgaonkar@users.noreply.github.com>
2023-12-20 18:42:55 +0530

    Add files via upload

---
+++ history.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Wed Jun  7 14:21:04 2023)---
+runfile('C:/Users/Rushikesh S. P/.spyder-py3/temp.py', wdir='C:/Users/Rushikesh S. P/.spyder-py3')
+
+## ---(Wed Jun  7 14:54:31 2023)---
+runfile('C:/Users/Rushikesh S. P/.spyder-py3/VR.py', wdir='C:/Users/Rushikesh S. P/.spyder-py3')
+
+## ---(Thu Jun  8 11:41:20 2023)---
+runfile('C:/Users/Rushikesh S. P/.spyder-py3/VR.py', wdir='C:/Users/Rushikesh S. P/.spyder-py3')
+
+## ---(Mon Jun 12 11:57:21 2023)---
+runfile('C:/Users/Rushikesh S. P/.spyder-py3/VR.py', wdir='C:/Users/Rushikesh S. P/.spyder-py3')
+
+## ---(Tue Jun 13 14:02:50 2023)---
+runfile('C:/Users/Rushikesh S. P/.spyder-py3/untitled0.py', wdir='C:/Users/Rushikesh S. P/.spyder-py3')
+
+## ---(Thu Sep 14 22:08:28 2023)---
+runfile('C:/Users/Rushikesh S. P/.spyder-py3/temp.py', wdir='C:/Users/Rushikesh S. P/.spyder-py3')
+
+## ---(Wed Dec 20 18:31:38 2023)---
+runfile('C:/Users/Rushikesh S. P/.spyder-py3/temp.py', wdir='C:/Users/Rushikesh S. P/.spyder-py3')
